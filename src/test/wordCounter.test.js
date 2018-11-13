1a764d74bf2e947b883a6ba9e58695cd6b6384e5
Bastien DURAN <bastienduran@mac.com>
2018-11-13 21:54:01 +0100

    handle repetitions

--- src/test/wordCounter.test.js
+++ src/test/wordCounter.test.js
@@ -7,3 +7,7 @@ test("find two word", () => {
   const text = "mot test";
   expect(wordCounter(text)).toEqual({ mot: 1, test: 1 });
 });
+test("find two word, one is doubled", () => {
+  const text = "mot mot test";
+  expect(wordCounter(text)).toEqual({ mot: 2, test: 1 });
+});
--- src/wordCounter.js
+++ src/wordCounter.js
@@ -7,6 +7,8 @@ const wordCounter = text => {
   return getMatchRegex(text).reduce((acc, curr) => {
     if (typeof acc[curr] === "undefined") {
       acc[curr] = 1;
+    } else {
+      acc[curr] += 1;
     }
     return acc;
   }, {});
