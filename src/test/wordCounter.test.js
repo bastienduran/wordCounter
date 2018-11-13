d2f5cc9165ca9ab77a66a508dc82203d01c05812
Bastien DURAN <bastienduran@mac.com>
2018-11-13 21:56:40 +0100

    xfind words with dash

--- src/test/wordCounter.test.js
+++ src/test/wordCounter.test.js
@@ -1,13 +1,26 @@
 import wordCounter from "../wordCounter";
+
 test("find one word", () => {
-  const text = "mot";
-  expect(wordCounter(text)).toEqual({ mot: 1 });
+  const text = "word";
+  expect(wordCounter(text)).toEqual({ word: 1 });
 });
+
 test("find two word", () => {
-  const text = "mot test";
-  expect(wordCounter(text)).toEqual({ mot: 1, test: 1 });
+  const text = "word test";
+  expect(wordCounter(text)).toEqual({ word: 1, test: 1 });
 });
+
 test("find two word, one is doubled", () => {
-  const text = "mot mot test";
-  expect(wordCounter(text)).toEqual({ mot: 2, test: 1 });
+  const text = "word word test";
+  expect(wordCounter(text)).toEqual({ word: 2, test: 1 });
+});
+
+test("find words with dash", () => {
+  const text = "word word test word-dash";
+  expect(wordCounter(text)).toEqual({ word: 2, test: 1, "word-dash": 1 });
+});
+
+test("handles digit in numbers", () => {
+  const text = "wo2rd word test word-dash";
+  expect(wordCounter(text)).toEqual({ word: 1, test: 1, "word-dash": 1 });
 });
--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,9 +1,8 @@
-const pattern = /\b[^\d\W]+\b/g;
+const pattern = /\b[^\d\W]+(-[^\d\W]+)?\b/g;
 
 const getMatchRegex = text => text.match(pattern);
 
 const wordCounter = text => {
-  console.log(getMatchRegex(text));
   return getMatchRegex(text).reduce((acc, curr) => {
     if (typeof acc[curr] === "undefined") {
       acc[curr] = 1;
